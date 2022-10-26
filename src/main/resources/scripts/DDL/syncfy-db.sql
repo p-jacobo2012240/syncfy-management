a38b8a0f678500dace96ad7f7350cacab7642577
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-10-26 17:35:29 -0600

    added scripts using for nestjs micro-service

--- src/main/resources/scripts/DDL/syncfy-db.sql
+++ src/main/resources/scripts/DDL/syncfy-db.sql
@@ -29,3 +29,19 @@ create table tbl_db_notification(
 	is_filter boolean,
 	auth_id integer
 )
+
+-- nestjs service
+
+create table tbl_db_task (
+	task_id serial primary key not null,
+	name varchar(40),
+	run_each varchar(100),
+	created_at date
+)
+
+
+create table tbl_db_task_detail (
+	task_detail_id serial primary key not null,
+	alert_id integer,
+	notification_id integer
+)

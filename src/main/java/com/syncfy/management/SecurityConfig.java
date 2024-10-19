52f474f09ce3356b62552931f5dbbb80fc44b35b
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-10-18 20:12:57 -0600

    keycloack initial config

---
+++ src/main/java/com/syncfy/management/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.syncfy.management;
+
+
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()  // Public access TEMP
+                        .anyRequest().authenticated()               // Authenticated access for others
+                )
+                .formLogin();  // Default login form
+        return http.build();
+    }
+}

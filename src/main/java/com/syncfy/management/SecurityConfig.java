69fa4f3b6d7a784d6b6e3c95343138cae74f06bb
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-10-23 17:52:49 -0600

    updated keycloack config

--- src/main/java/com/syncfy/management/SecurityConfig.java
+++ src/main/java/com/syncfy/management/SecurityConfig.java
@@ -1,25 +1,22 @@
 package com.syncfy.management;
 
-
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()  // Public access TEMP
-                        .anyRequest().authenticated()               // Authenticated access for others
-                )
-                .formLogin();  // Default login form
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/**"))
+                .authorizeHttpRequests(httpRequests -> httpRequests.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+                .build();
     }
 }
